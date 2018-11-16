1175cdbb0fcc43b617d86e5bab0648903b3c3359
sashokrist <alexander.krist@gmail.com>
2018-11-17 00:58:16 +0100

    Create Dashboard view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -22,6 +24,8 @@ class HomeController extends Controller
         return view('profile');
     }
     public function getDashboard(){
+
         return view('/profile');
     }
+
 }
